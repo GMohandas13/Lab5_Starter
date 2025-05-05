c691a8cb50cac9093a8c91e9da2c664d593dfec1
Aarush Mehrotra <43413800+iAarush@users.noreply.github.com>
2025-05-05 16:06:17 -0700

    Images load now

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,32 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const hornSelect = document.getElementById("horn-select"); 
+  const hornImageTop = document.getElementById("expose"); 
+  const hornImage = hornImageTop.querySelector('img'); 
+
+  const hornAudio = document.querySelector("audio"); 
+  
+  hornSelect.addEventListener('change', () => {
+    const selectedValue = hornSelect.value; 
+
+    if (selectedValue == 'air-horn') {
+      hornAudio.src = 'assets/audio/air-horn.mp3';
+      hornImage.src = 'assets/images/air-horn.svg'; 
+    }
+    if (selectedValue == 'car-horn') {
+      hornAudio.src = 'assets/audio/car-horn.mp3'; 
+      hornImage.src = 'assets/images/car-horn.svg'; 
+    }
+    if (selectedValue == 'party-horn') {
+      hornAudio.src = 'assets/audio/party-horn.mp3'; 
+      hornImage.src = 'assets/images/party-horn.svg'; 
+    }
+  })
+
+  /*
+  hornImage.src = f""; /*this should be the variable for the selected form*//*
+  document.images[0]; 
+  document.getElementById("horn-select");
+  */
 }
