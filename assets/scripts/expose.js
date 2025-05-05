ac863ecdd71cffc44646e138ec6a75d72d739c67
gautammdas13 <gmohandas2@student.ohlone.edu>
2025-05-05 16:11:00 -0700

    Added Volume Slider Function

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,10 +3,29 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  let selection = document.getElementById("horn-select"); 
-  
-  selection.addEventListener("change", () => { });
+  //volume section 
+  const volumeLevel = document.getElementById("volume");
+  const volumeImg = document.querySelector('#volume-controls img');
+  const audio = document.querySelector('audio');
 
-  oninput = (event) => { }
+  volumeLevel.addEventListener('input', changeLevel);
 
+  function changeLevel(event) {
+    const value = Number(volumeLevel.value);
+
+    audio.volume = value / 100;
+
+    if (value === 0){
+      volumeImg.src = 'assets/icons/volume-level-0.svg';
+    }
+    else if (value < 34) {
+      volumeImg.src = 'assets/icons/volume-level-1.svg';
+    }
+    else if (value < 67) {
+      volumeImg.src = 'assets/icons/volume-level-2.svg';
+    }
+    else {
+      volumeImg.src = 'assets/icons/volume-level-3.svg';
+    }
+  }
 }
