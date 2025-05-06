9b0cac1f54b8740ab88ca8de008d5523291e051a
gautammdas13 <gmohandas2@student.ohlone.edu>
2025-05-06 15:22:46 -0700

    Added Button Function and Speaking

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,6 +3,9 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   const voiceSelect = document.getElementById('voice-select');
+  const textToSpeak = document.getElementById('text-to-speak');
+  const button = document.querySelector('button');
+  const face = document.querySelector('#explore img');
 
   function addVoices() {
     const voices = speechSynthesis.getVoices();
@@ -18,4 +21,25 @@ function init() {
 
   addVoices();
   speechSynthesis.addEventListener('voiceschanged', addVoices);
+
+  function textToSpeech() {
+    let utterance = new SpeechSynthesisUtterance(textToSpeak.value);
+    const voiceChoices = speechSynthesis.getVoices();
+    const voiceSelection = voiceSelect.value;
+
+    if (voiceSelection !== 'select'){
+      utterance.voice = voiceChoices[voiceSelection];
+    }
+
+    utterance.addEventListener('start', function () {
+      face.src = 'assets/images/smiling-open.png';
+    });
+
+    utterance.addEventListener('end', function () {
+      face.src = 'assets/images/smiling.png';
+    });
+
+    speechSynthesis.speak(utterance);
+  }
+  button.addEventListener('click', textToSpeech);
 }
