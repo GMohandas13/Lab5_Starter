d5d7f00647d890810cc939041513596acc4a30fc
Aarush Mehrotra <43413800+iAarush@users.noreply.github.com>
2025-05-06 15:42:53 -0700

    isPhoneNumber unit tests added

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -9,3 +9,23 @@ import {
 } from '../code-to-unit-test/unit-test-me';
 
 // TODO - Part 2
+
+//tests for isPhoneNumber
+test('phone number should be 7 or 10 digits long', () => {
+  expect(isPhoneNumber('9998887777').toBe(true)); 
+  expect(isPhoneNumber('456-7890').toBe(true));
+});
+
+test('phone number should support parentheses', () => {
+  expect(isPhoneNumber('1234567890').toBe(true));
+  expect(isPhoneNumber('(123) 456-7890').toBe(true)); 
+});
+
+test('phone number should not support letters', () => {
+  expect(isPhoneNumber('hi!').toBe(false));
+});
+
+test('phone number should not support other lengths', () => {
+  expect(isPhoneNumber('123').toBe(false))
+  expect(isPhoneNumber('12345678900987654321').toBe(false));
+}); 
